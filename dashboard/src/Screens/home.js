03f52f38473d72cd6d2ca31eee528be38eda1a10
mamba099 <nzakajf09@gmail.com>
2022-11-24 18:44:42 +0200

    initial commit

--- dashboard/src/Screens/home.js
+++ dashboard/src/Screens/home.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
 const home = () => {
   return (
-    <div>
-        home
-        <p>How are you i am her</p>
-    </div>
+    <>
+      <Sidebar/>
+      <main className='main-wrap'>
+          <Header/>
+      </main>
+    </>
   )
 }
 
